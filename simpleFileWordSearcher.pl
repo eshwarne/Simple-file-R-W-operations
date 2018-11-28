276d9fd3b26a18029b01e4fd243f5cb516aab624
eshwarne <northeasteditsofficial@gmail.com>
2018-11-28 16:47:06 +0530

    sub routine done

--- simpleFileWordSearcher.pl
+++ simpleFileWordSearcher.pl
@@ -3,21 +3,39 @@
 
 =cut
 
-print("\U The following code allows you to search for a specific word in a file and return its count\n")
+print("\U The following code allows you to search for a specific word in a file and return its count\n");
 sub Counter{
     $hashed=($_[0]=0);
-    open(DATA,"<".$_[1]."txt");
+    chomp($_[1]);
+    $fn="<".$_[1];
+    $fn=$fn.".txt";
+    print($fn);
+    open(DATA,$fn);
     while($data=<DATA>){
-        if(exists($hashed{$_[0]}){}){
-            $hashed{$_[0]}+=1;
+        @splitting_array=split(" ",$data);
+        while($i = each @splitting_array){
+            if(exists($hashed{$splitting_array[$i]})){
+                $hashed{$_[0]}+=1;
         }
         else{
-            next;
+                next;
         }
+        }
+       
     }
-    return %hashed
+    return %hashed;
 }
 print("\U keyword to search: ");
 $key=<>;
 print("\U File name        : ");
 $fp=<>;
+%key_count=&Counter($key,$fp);
+if($key_count{$key}==0){
+    print("THE WORD DOES NOT EXIST");
+}
+else{
+    print("FOUND IT!\n It occured ".$key_count{$key}." times");
+}
+while($key = each %key_count){
+    print $key_count{$key};
+}
